004b5af39b10ec9b73da0e27809db09463a3f037
vas1l3v <pvasilev9525@gmail.com>
2021-04-07 07:27:21 +0000

    initial commit

---
+++ Contoso.Apps.SportsLeague.Web/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Contoso.Apps.SportsLeague.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Controllers\AccountController.cs
+
+        private string _editProfilePolicyId;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _editProfilePolicyId = configuration.GetValue<string>("AzureADB2C:EditProfilePolicyId");
+        }
+
+        public ActionResult SignIn()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Challenge(new AuthenticationProperties() { RedirectUri = "/signin-oidc-b2c" }, AzureADB2CDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult SignUp()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Challenge(new AuthenticationProperties() { RedirectUri = "/signin-oidc-b2c" }, AzureADB2CDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Profile()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var properties = new AuthenticationProperties() { RedirectUri = "/" };
+                properties.Items[AzureADB2CDefaults.PolicyKey] = _editProfilePolicyId;
+                return Challenge(
+                    properties,
+                    AzureADB2CDefaults.AuthenticationScheme);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult SignOut()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            string redirectUri = Url.Action("Index", "Home", null, Request.Scheme);
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = redirectUri
+            };
+            return SignOut(properties, AzureADB2CDefaults.CookieScheme, AzureADB2CDefaults.OpenIdScheme);
+        }
+    }
+}
